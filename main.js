d71e912cd3daac1208e4f455d2777cc03ef325b4
Neha <neharaao2022@gmail.com>
2023-12-20 16:42:36 +0530

    made a word in back card

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 const container = document.querySelector(".main-container")
 const crouselImg = document.querySelector(".crousel-img")
+const dots = document.querySelectorAll(".dot")
 // console.log(crouselImg)
 let i = 0;
 let alphabet;
 
 // crousel ======================================================
-
 let crousel = [
+   "imges/crousel/crousel.jpg",
    "imges/crousel/crousel1.jpg",
-   "imges/crousel/crousel2.jpeg",
-   "imges/crousel/crousel3.jpeg"
+   "imges/crousel/crousel2.jpg"
 ]
 autoplay = setInterval(function () {
    i++;
+   let selectedDotId = "#dot" + i;
+   let selectedDot = document.querySelector(selectedDotId);
+
+   dots.forEach(dot => dot.style.backgroundColor = "white");
+   selectedDot.style.backgroundColor = "black";
    if (i > 2)
       i = 0;
    crouselImg.setAttribute("src", crousel[i])
 }, 1200);
 
-// frontPattern=============================
+
+// frontPattern=========================== is liye lgaye hai kyoki hame 6 ke bad fir se vahi pattern lana hai==
 
 let frontPattern = [
    "pattern1", "pattern2", "pattern3", "pattern4", "pattern5", "pattern6"
-
 ]
 
-for (let i = 65, j = 0; i <= 90; i++, j++) {
+// soundArray=============================== is liye bnaye hai taki usko pta chale ki use bolna kya hai
+
+let soundArray = ["Apple", "Ball", "Cat", "Dog", "Elephant", "Fish", "Goat", "Hourse", "Igloo", "Joker", "Kite", "Lion", "Monkey", "Nose", "Octopus", "Pig", "Queen", "Rocket", "Snake", "Tiger", "Umbrella", "Van", "Watermelon", "X-ray", "Yak", "zebra"]
+
+// loop chala rhe hai i vala loop image ke liye chal rha hai j vala loop pattern ke liye chal rha hai and k vala loop sound ke liye chal rha hai
+
+for (let i = 65, j = 0, k = 0; i <= 90; i++, j++, k++) {
    if (j >= 6)
       j = 0;
    alphabet = String.fromCharCode(i)
@@ -34,28 +45,28 @@ for (let i = 65, j = 0; i <= 90; i++, j++) {
    let backDiv = document.createElement("div")
    container.appendChild(frontCard)
    container.appendChild(backDiv)
+  
    // frontCard.setAttribute("class", "front-card");
    backDiv.setAttribute("class", "back-card")
-   //    console.log(container)
    frontCard.classList.add("front-card")
    frontCard.classList.add(frontPattern[j])
 
-// text==============================
+   // text==============================
 
    let text = document.createElement("p")
    text.setAttribute("class", "text")
    frontCard.appendChild(text)
    text.innerHTML = alphabet;
 
-// backImg=======================================
+   // backImg=======================================
 
    let imageArr = document.createElement("img")
-   imageArr.setAttribute("src", `imges/alphabets-img/${alphabet.toLocaleLowerCase()}.png`)
+   imageArr.setAttribute("src", `imges/alphabets-img/${alphabet.toLocaleLowerCase()}.jpeg`)
    imageArr.setAttribute("class", "img-class")
    backDiv.appendChild(imageArr)
    //   console.log(imageArr)
 
-// flipCard=================================
+   // flipCard=================================
 
    let flipCard = document.createElement("div")
    flipCard.appendChild(frontCard)
@@ -63,11 +74,22 @@ for (let i = 65, j = 0; i <= 90; i++, j++) {
    container.appendChild(flipCard)
    flipCard.setAttribute("class", "flip-card")
    // console.log(flipCard);
-
+   
+ // ---------------spelling------------------
+   let word = document.createElement("p")
+   word.setAttribute("class","words")
+   word.innerHTML= soundArray[k];
+   backDiv.appendChild(word);
 
    flipCard.addEventListener("click", () => {
       flipCard.classList.toggle("flipped")
+
+      let msg = new SpeechSynthesisUtterance();
+      msg.text = soundArray[k];
+      window.speechSynthesis.speak(msg);
+      console.log(msg)
    })
 
-
 }
+
+
